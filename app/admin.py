1ffa1c32ddeffd993615be6408a0456897b07149
iamjoseph1998 <iamjoseph1998@gmail.com>
2023-02-03 20:01:42 +0530

    Added profile data model and serializer

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from app.models import Profile
 
-# Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['email', 'first_name', 'last_name', 'phone']
+    list_display_links = ['email', 'first_name', 'last_name', 'phone']
+    list_filter = ['email', 'first_name', 'last_name']
+    readonly_fields = ['id']
+
+    fieldsets = (
+        ('Profile Info', {'fields': ('id', 'first_name', 'last_name', 'email', 'phone', 'address'),}),
+    )
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+admin.site.register(Profile, ProfileAdmin)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator, validate_email
 
-# Create your models here.
+
+phone_regex = RegexValidator(
+    regex = r"^\d{10}", message="Phone number must be 10 digits only."
+)
+
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=15)
+    email = models.EmailField(validators=[validate_email])
+    phone = models.CharField(max_length=10, validators=[phone_regex])
+    address = models.TextField()
+    photo = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.email
+
+    def clean(self):
+        super().clean()
+        if not self.id and Profile.objects.exists():
+            raise ValidationError('You can only add a single profile.')
+
+    
+    class Meta:
+        verbose_name_plural = 'Profile'
